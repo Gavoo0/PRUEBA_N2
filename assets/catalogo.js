42a98212a9c45c2773d7b9577612913f7d205c72
Saidai21 <max.valdebenit6@gmail.com>
2024-05-16 18:18:37 -0400

    arreglado entrea comillas

--- assets/catalogo.js
+++ assets/catalogo.js
@@ -10,6 +10,14 @@
               const buttonControlled = $(`[data-target="#${$(this).attr('id')}"]`);
               buttonControlled.attr('aria-expanded', 'false');
             }
+            
           });
         });
       });
+      $(document).ready(function(){
+        // Función para manejar el evento click del botón "Mostrar todas las bicicletas"
+        $('#showAllBikes').click(function () {
+          $('.collapse').collapse('show');
+        });
+      });
+      
