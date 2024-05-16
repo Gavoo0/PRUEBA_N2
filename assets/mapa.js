22e368d1ce3f3d8d2af726adf39ce2dd84fb3846
Gavoo0 <gabrieli.godoyl@gmail.com>
2024-05-16 19:17:13 -0400

    cambios

--- assets/mapa.js
+++ assets/mapa.js
@@ -1,16 +1,15 @@
 function initMap() {
     const map = L.map('map').setView([-1.286389, 36.817223], 13); // Coordenadas de Nairobi, Kenia
 
-    // Añadir la capa de mapa de OpenStreetMap
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    // Solicitar datos de BikeWise API
+    
     $.ajax({
         url: 'https://bikewise.org:443/api/v2/incidents',
         data: {
-            incident_type: 'theft', // Tipo de incidente (robo)
+            incident_type: 'theft', 
             proximity: 'Nairobi, Kenya', // Ubicación de proximidad
             proximity_square: 50 // Radio de proximidad en km
         },
